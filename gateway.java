ffffcf0a614c4bc79e2882012454c3387d267365
skylible <37142188+skylible@users.noreply.github.com>
2019-05-20 21:29:16 +0700

    Main Gateway Computer
    This gateway receive data from arduino client through serial port and send it to the Thingsboard API through HTTP json request.

---
+++ gateway.java
@@ -0,0 +1,147 @@
+/*
+    Made by Widi
+ */
+package arduino;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+import java.io.IOException;
+import java.util.Enumeration;
+
+// internet stuff here
+import java.net.*;
+
+public class SerialTest implements SerialPortEventListener {
+
+    SerialPort serialPort;
+    /**
+     * The port we're normally going to use.
+     */
+    private static final String PORT_NAMES[] = {"/dev/tty.usbserial-A9007UX1", // Mac OS X
+        "/dev/ttyUSB0", // Linux
+        "COM7", // Windows
+};
+    private BufferedReader input;
+    private OutputStream output;
+    private static final int TIME_OUT = 2000;
+    private static final int DATA_RATE = 115200;
+
+// Internet stuff
+    private static String link = "https://demo.thingsboard.io/api/v1/Jlpkzg3VHdeUOXGmGDNv/telemetry";
+
+    public void initialize() {
+        CommPortIdentifier portId = null;
+        Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+
+        //First, Find an instance of serial port as set in PORT_NAMES.
+        while (portEnum.hasMoreElements()) {
+            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+            for (String portName : PORT_NAMES) {
+                if (currPortId.getName().equals(portName)) {
+                    portId = currPortId;
+                    break;
+                }
+            }
+        }
+        if (portId == null) {
+            System.out.println("Could not find COM port.");
+            return;
+        }
+
+        try {
+            serialPort = (SerialPort) portId.open(this.getClass().getName(),
+                    TIME_OUT);
+            serialPort.setSerialPortParams(DATA_RATE,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+
+            // open the streams
+            input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+            output = serialPort.getOutputStream();
+
+            serialPort.addEventListener(this);
+            serialPort.notifyOnDataAvailable(true);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
+    }
+
+    public synchronized void close() {
+        if (serialPort != null) {
+            serialPort.removeEventListener();
+            serialPort.close();
+        }
+    }
+
+    public synchronized void serialEvent(SerialPortEvent oEvent) {
+        if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+            try {
+                String inputLine = null;
+                if (input.ready()) {
+                    inputLine = input.readLine();
+                    System.out.println(inputLine);
+                    sendJSON(inputLine);
+                }
+
+            } catch (Exception e) {
+                System.err.println(e.toString());
+            }
+        }
+        // Ignore all the other eventTypes, but you should consider the other ones.
+    }
+
+    public static void main(String[] args) throws Exception {
+        SerialTest main = new SerialTest();
+        main.initialize();
+        Thread t = new Thread() {
+            public void run() {
+                //the following line will keep this app alive for 1000    seconds,
+                //waiting for events to occur and responding to them    (printing incoming messages to console).
+                try {
+                    Thread.sleep(1000000);
+                } catch (InterruptedException ie) {
+                }
+            }
+        };
+        t.start();
+        System.out.println("Started");
+    }
+
+// INTERNET STUFF BELOW
+    public static void sendJSON(String data) throws IOException {
+        URL url = null;
+        try {
+            url = new URL(link);
+        } catch (MalformedURLException ex) {
+            System.out.println(ex);
+        }
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+        String jsonInputString = "{\"consumption\":\"" + data + "\"}";
+
+        try (OutputStream os = con.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        //reading response
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+//            System.out.println(response.toString());
+        }
+    }
+}
